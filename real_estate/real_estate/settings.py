1567e5cdc0f6a3d4986b22c228e03aa4a2616dc6
trungkien2509 <kienle.itjob@gmail.com>
2022-02-23 16:50:18 +0700

    ver1

--- real_estate/real_estate/settings.py
+++ real_estate/real_estate/settings.py
@@ -82,8 +82,12 @@ WSGI_APPLICATION = 'real_estate.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'mydb',
+        'USER': 'root',
+        'PASSWORD': 'wearethebest1',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
     }
 }
 
